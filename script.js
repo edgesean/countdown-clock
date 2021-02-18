ea35a86da99c85e4fa68410939ce96a1703d59a9
Sean Dillon <seandillon59@gmail.com>
2021-02-18 20:13:57 +0100

    Fully functioning
    Counts down time until Codeworks March cohort starts.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('test');
+
+
+
+
+var endDate = new Date("Mar 1, 2021 09:00:00").getTime();
+
+setInterval(function(){
+var now = new Date().getTime();
+console.log(now);
+var distance = endDate - now;
+
+document.getElementById("days").innerHTML = Math.floor(distance / (1000 * 60 * 60 * 24)) + ' Days';
+document.getElementById("hours").innerHTML = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + ' Hours';
+document.getElementById("minutes").innerHTML = ''+ Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) +' Minutes';
+document.getElementById("seconds").innerHTML = ''+ Math.floor((distance % (1000 * 60)) / 1000) +' Seconds';
+
+      }, 1000);
+
+
+
